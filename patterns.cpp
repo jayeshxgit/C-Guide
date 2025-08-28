8649c934b31b4cb3241459345b10ca4318cb6a99
Jayesh Kapoor <Jayeshkapoor169@gmail.com>
2025-08-28 13:02:13 +0530

    Update patterns.cpp

--- patterns.cpp
+++ patterns.cpp
@@ -41,7 +41,7 @@ cin>>n;
         cout<<endl;
 }
 // odd triangle.
-
+// External variable method.
 int n;
 cin>>n;
  for (int i=1;i<=n;i++){ // i is telling the number of rows
@@ -53,7 +53,7 @@ cin>>n;
         cout<<endl;
 }
 // method 2(best method)
-
+// Iteration manipulation Method.
 int n;
 cin>>n;
  for (int i=1;i<=n;i++){ 
